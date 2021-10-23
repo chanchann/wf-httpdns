b6f0994554f647c887de890f98d9edfe2b64b886
chanchann <www.769440972@qq.com>
2021-10-23 13:13:28 +0800

    fix some bugs

--- src/server/HDService.h
+++ src/server/HDService.h
@@ -43,9 +43,6 @@ private:
 	template<typename T>
 	static void check_query_field(T *ctx,
 								  std::map<std::string, std::string> &query_split);
-    
-    template<typename T>
-    static bool split_host_port(T *ctx, const std::string& url);
  
 };
 
@@ -75,28 +72,5 @@ void HDService::check_query_field(T *ctx,
     }
 }
 
-template<typename T>
-bool HDService::split_host_port(T *ctx, const std::string& url) 
-{
-    // www.baidu.com[:80]
-    auto host_port = StringUtil::split(url, ':');
-    if(host_port.size() == 0)
-    {
-        // In fact, here has already been checked by `check_host_field`
-        spdlog::error("url is invalid");
-        return false;
-    } 
-    else if(host_port.size() == 2) 
-    {
-        ctx->host = host_port[0];
-        ctx->port = static_cast<unsigned int>(std::stoul(host_port[1]));
-    } 
-    else 
-    {
-        ctx->host = host_port[0];
-        ctx->port = 80;   // default http port 80
-    }
-    return true;
-}
 
 #endif // _HDSERVICE_H_
